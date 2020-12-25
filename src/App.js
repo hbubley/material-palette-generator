e392f08b8f7ddbe2cd243992aedc871baee3d3f7
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-12-25 09:27:23 -0600

    Created palette component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Palette from "./components/ColorPalette/Palette";
+import { SEED_PALETTES } from "./utils/seedPalettes";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Palette palette={SEED_PALETTES[3]} />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/ColorPalette/Palette.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Palette = ({palette}) => {
+    return (
+        <div className="palette">
+            <h1>palette</h1>
+            <div className="palette-colors">Colors</div>
+        </div>
+    );
+};
+
+export default Palette;
